e3e26856c1bb65c2ca50e897bb16288d11e9ee88
Anand A Joshi <ajoshi@usc.edu>
2021-09-20 13:42:21 -0700

    update

--- src/main_lin_regression_oasis3.py
+++ src/main_lin_regression_oasis3.py
@@ -16,6 +16,7 @@ from grayord_utils import visdata_grayord, vis_grayord_sigpval
 # s for the data and BFP software
 from tqdm import tqdm
 import time
+import glob
 
 BFPPATH = '/home/ajoshi/projects/bfp'
 FSL_PATH = '/usr/share/fsl/5.0'
@@ -34,61 +35,65 @@ NUM_SUB = 350  # Number of subjects for the study
 
 def main():
 
-    print('Reading subjects')
+    s = glob.glob('/home/ajoshi/projects/AD_processing/csv_files/*.csv')
 
-    _, reg_var, sub_files = read_oasis3_data(
-        csv_fname=CSV_FILE,
-        data_dir=DATA_DIR,
-        reg_var_name='UDSB9',  #'Verbal IQ',  #  #
-        num_sub=NUM_SUB,
-        len_time=LEN_TIME)
+    for i in range(len(s)):
 
-    # Shuffle reg_var and subjects for testing
-    #reg_var = sp.random.permutation(reg_var)
-    #ran_perm = sp.random.permutation(len(reg_var))
-    #reg_var = reg_var
-    #sub_files = [sub_files[i] for i in range(len(reg_var))]
+        measure = s[i][51:-4]
+        CSV_FILE = s[i]
 
-    sub_files=sub_files #[50:100]
-    reg_var = reg_var #[50:100]
-    t0 = time.time()
-    print('performing stats based on random pairwise distances')
+        print('Reading subjects')
+        _, reg_var, sub_files = read_oasis3_data(csv_fname=CSV_FILE,
+                                                 data_dir=DATA_DIR,
+                                                 reg_var_name=measure,
+                                                 num_sub=NUM_SUB,
+                                                 len_time=LEN_TIME)
 
-    corr_pval_max, corr_pval_fdr, rho, power, effect, estN = randpairs_regression(
-        bfp_path=BFPPATH,
-        sub_files=sub_files,
-        reg_var=reg_var,
-        num_pairs=20000,  # 19900,
-        nperm=2000,
-        len_time=LEN_TIME,
-        num_proc=6,
-        pearson_fdr_test=False)
-    t1 = time.time()
+        # Shuffle reg_var and subjects for testing
+        #reg_var = sp.random.permutation(reg_var)
+        #ran_perm = sp.random.permutation(len(reg_var))
+        #reg_var = reg_var
+        #sub_files = [sub_files[i] for i in range(len(reg_var))]
 
-    print(t1 - t0)
-    np.savez(
-        'pval_num_pairs20000_nsub350_nperm2000.npz',
-        corr_pval_max=corr_pval_max,
-        corr_pval_fdr=corr_pval_fdr,
-        rho=rho, power=power, effect=effect, estN=estN)
-    # corr_pval_max=a['corr_pval_max']
-    # corr_pval_fdr=a['corr_pval_fdr']
-    vis_grayord_sigpval(
-        corr_pval_max,
-        surf_name='rand_dist_corr_perm_pairs20000_max',
-        out_dir='.',
-        smooth_iter=1000,
-        bfp_path=BFPPATH,
-        fsl_path=FSL_PATH,
-        sig_alpha=0.05)
-    vis_grayord_sigpval(
-        corr_pval_fdr,
-        surf_name='rand_dist_corr_perm_pairs20000_fdr',
-        out_dir='.',
-        smooth_iter=1000,
-        bfp_path=BFPPATH,
-        fsl_path=FSL_PATH,
-        sig_alpha=0.05)
+        sub_files = sub_files
+        reg_var = reg_var
+        t0 = time.time()
+        print('performing stats based on random pairwise distances')
+
+        corr_pval_max, corr_pval_fdr, rho, power, effect, estN = randpairs_regression(
+            bfp_path=BFPPATH,
+            sub_files=sub_files,
+            reg_var=reg_var,
+            num_pairs=20000,
+            nperm=2000,
+            len_time=LEN_TIME,
+            num_proc=6,
+            pearson_fdr_test=False)
+        t1 = time.time()
+
+        print(t1 - t0)
+        np.savez('pval_num_pairs20000_nsub350_nperm2000_' + measure + '.npz',
+                 corr_pval_max=corr_pval_max,
+                 corr_pval_fdr=corr_pval_fdr,
+                 rho=rho,
+                 power=power,
+                 effect=effect,
+                 estN=estN)
+
+    vis_grayord_sigpval(corr_pval_max,
+                        surf_name='rand_dist_corr_perm_pairs20000_max',
+                        out_dir='.',
+                        smooth_iter=1000,
+                        bfp_path=BFPPATH,
+                        fsl_path=FSL_PATH,
+                        sig_alpha=0.05)
+    vis_grayord_sigpval(corr_pval_fdr,
+                        surf_name='rand_dist_corr_perm_pairs20000_fdr',
+                        out_dir='.',
+                        smooth_iter=1000,
+                        bfp_path=BFPPATH,
+                        fsl_path=FSL_PATH,
+                        sig_alpha=0.05)
 
     print('Results saved')
 

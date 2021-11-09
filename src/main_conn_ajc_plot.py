c42e3b854ca128301101dc6784065a80fc6f1db4
Anand A Joshi <ajoshi@masal.usc.edu>
2021-11-08 23:36:36 -0800

    update

--- src/main_conn_ajc_plot.py
+++ src/main_conn_ajc_plot.py
@@ -56,39 +56,45 @@ def main():
     conn_mat = a['conn_mat']
     conn_pen = conn_mat
 
+    fig, axes = plt.subplots(nrows=1, ncols=3)
 
-    fig, axes = plt.subplots(nrows=2, ncols=2)
+#    ax0 = axes.flat[0]
+#    im = ax0.imshow(conn_rest,vmin=-1, vmax=1)
+#    ax0.set_title('Rest')
+    # ax0.set_xticks(range(conn_mat.shape[0]))
+    # ax0.set_yticks(range(conn_mat.shape[1]))
 
-    ax0 = axes.flat[0]
-    im = ax0.imshow(conn_rest,vmin=-1, vmax=1)
-    ax0.set_title('Rest')
-    #ax0.set_xticks(range(conn_mat.shape[0]))
-    #ax0.set_yticks(range(conn_mat.shape[1]))
+    ax1 = axes.flat[0]
+    im = ax1.imshow(100*np.abs(conn_tap-conn_rest) /
+                    np.abs(conn_rest), vmin=0, vmax=100, cmap='hot')
+    ax1.set_title('Finger Tap', fontsize=14)
+    ax1.tick_params(axis='both', which='major', labelsize=14)
 
+    # ax1.set_xticks(range(conn_mat.shape[0]))
+    # ax1.set_yticks(range(conn_mat.shape[1]))
 
-    ax1 = axes.flat[1]
-    im = ax1.imshow(conn_tap,vmin=-1, vmax=1)
-    ax1.set_title('Finger Tap')
-    #ax1.set_xticks(range(conn_mat.shape[0]))
-    #ax1.set_yticks(range(conn_mat.shape[1]))
+    ax2 = axes.flat[1]
+    im = ax2.imshow(100*np.abs(conn_reach-conn_rest) /
+                    np.abs(conn_rest), vmin=0, vmax=100, cmap='hot')
+    ax2.set_title('Reach', fontsize=14)
+    ax2.tick_params(axis='both', which='major', labelsize=14)
 
-    ax2 = axes.flat[2]
-    im = ax2.imshow(conn_reach,vmin=-1, vmax=1)
-    ax2.set_title('Reach')
-    #ax2.set_xticks(range(conn_mat.shape[0]))
-    #ax2.set_yticks(range(conn_mat.shape[1]))
+    # ax2.set_xticks(range(conn_mat.shape[0]))
+    # ax2.set_yticks(range(conn_mat.shape[1]))
 
-    ax3 = axes.flat[3]
-    im = ax3.imshow(conn_pen,vmin=-1, vmax=1)
-    ax3.set_title('Pen Hold')
-    #ax2.set_xticks(range(conn_mat.shape[0]))
-    #ax2.set_yticks(range(conn_mat.shape[1]))
+    ax3 = axes.flat[2]
+    im = ax3.imshow(100*np.abs(conn_pen-conn_rest) /
+                    np.abs(conn_rest), vmin=0, vmax=100, cmap='hot')
+    ax3.set_title('Pen Hold', fontsize=14)
+    ax3.tick_params(axis='both', which='major', labelsize=14)
 
-    plt.colorbar(im,ax=axes.ravel().tolist())
+    # ax2.set_xticks(range(conn_mat.shape[0]))
+    # ax2.set_yticks(range(conn_mat.shape[1]))
+
+    cbar = plt.colorbar(im, ax=axes.ravel().tolist())
+    cbar.ax.tick_params(labelsize=14)
 
     plt.show()
-
-
 
 
 if __name__ == "__main__":

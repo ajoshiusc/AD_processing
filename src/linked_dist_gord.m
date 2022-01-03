8938877c94bedfc7f078b532d28513c5315a95e5
Anand A. Joshi <ajoshi@usc.edu>
2022-01-03 13:08:05 -0800

    update

--- src/linked_dist_gord.m
+++ src/linked_dist_gord.m
@@ -1,27 +1,13 @@
 
-function linked_dist_gord(subbasename,GOrdSurfIndFile)
+function linked_dist_gord(subbasename,GOrdSurfIndFile,GOrdFile)
 
 left_inner = readdfs([subbasename,'.left.inner.cortex.svreg.dfs']);
 left_pial = readdfs([subbasename,'.left.pial.cortex.svreg.dfs']);
 right_inner = readdfs([subbasename,'.right.inner.cortex.svreg.dfs']);
 right_pial = readdfs([subbasename,'.right.pial.cortex.svreg.dfs']);
 
-[pth, sub] = fileparts(subbasename);
-atlas_left = readdfs(fullfile(pth,'atlas.left.mid.cortex.svreg.dfs'))
-atlas_right = readdfs(fullfile(pth,'atlas.right.mid.cortex.svreg.dfs'))
-
-left_ld.attributes = sqrt(sum((left_inner.vertices - left_pial.vertices).^2,2));
-right_ld.attributes = sqrt(sum((right_inner.vertices - right_pial.vertices).^2,2));
-
-writedfs([subbasename,'.left.mid.cortex.ld.dfs'],left_ld);
-writedfs([subbasename,'.right.mid.cortex.ld.dfs'],right_ld);
-
-atlas_left = map_data_flatmap(left_ld,left_ld,atlas_left);
-atlas_right = map_data_flatmap(right_ld,right_ld,atlas_right);
+left_data = sqrt(sum((left_inner.vertices - left_pial.vertices).^2,2));
+right_data = sqrt(sum((right_inner.vertices - right_pial.vertices).^2,2));
 
 
-writedfs(fullfile(pth,'atlas.left.mid.cortex.ld.dfs'),atlas_left);
-writedfs(fullfile(pth,'atlas.right.mid.cortex.ld.dfs'),atlas_right);
-
-
-generateGOrdSCT(subbasename, GOrdSurfIndFile);
+surfdata2gord(subbasename, left_data, right_data, GOrdSurfIndFile, GOrdFile)
---
+++ src/main_linked_dist_test.m
@@ -0,0 +1,12 @@
+clc;clear all;close all;restoredefaultpath;
+%addpath(genpath('/big_disk/ajoshi/coding_ground/bfp/supp_data'))
+addpath(genpath('/home/ajoshi/projects/bfp/src'));
+
+
+subbasename='/ImagePTE1/ajoshi/data/bfp_oasis3/sub-OAS30001_ses-d0129_run_02/anat/sub-OAS30001_ses-d0129_run_02_T1w';
+
+GOrdSurfIndFile = '/home/ajoshi/projects/bfp/supp_data/bci_grayordinates_surf_ind.mat';
+GOrdFile='temp.gord.mat';
+
+linked_dist_gord(subbasename,GOrdSurfIndFile,GOrdFile);
+

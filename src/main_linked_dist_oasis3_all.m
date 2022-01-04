efb012f64a242f6d4aa658fdd35d91e17fcfd796
Anand A. Joshi <ajoshi@usc.edu>
2022-01-03 20:07:09 -0800

    update

--- src/main_linked_dist_oasis3_all.m
+++ src/main_linked_dist_oasis3_all.m
@@ -3,12 +3,9 @@ clc;clear all;close all;restoredefaultpath;
 addpath(genpath('/home/ajoshi/projects/bfp/src'));
 %    1050345 rest 2
 
-studydir='/deneb_disk/bfp_oasis3';
-TR='';
-% Set the input arguments
-configfile='/home/ajoshi/projects/AD_processing/src/config_bfp_preproc.ini';
+studydir='/ImagePTE1/ajoshi/data/bfp_oasis3';
 
-subdir='/ImagePTE1/ajoshi/oasis3_bids';
+subdir='/ImagePTE1/ajoshi/data/oasis3_bids';
 l=dir(subdir);
 
 s=0;
@@ -28,15 +25,15 @@ for j=3:length(l)
     
     %fprintf('--------\n')
     
-    t1=fullfile('/ImagePTE1/ajoshi/oasis3_bids/',subname,sessions{1},'anat',[subname,'_',sessions{1},'_run-01_T1w.nii.gz']);
-    fmri=fullfile('/ImagePTE1/ajoshi/oasis3_bids/',subname,sessions{1},'func',[subname,'_',sessions{1},'_task-rest_run-02_bold.nii.gz']);
+    t1=fullfile('/ImagePTE1/ajoshi/data/oasis3_bids/',subname,sessions{1},'anat',[subname,'_',sessions{1},'_run-01_T1w.nii.gz']);
+    fmri=fullfile('/ImagePTE1/ajoshi/data/oasis3_bids/',subname,sessions{1},'func',[subname,'_',sessions{1},'_task-rest_run-02_bold.nii.gz']);
     
     if ~exist(t1,'file')
         t1=fullfile('/ImagePTE1/ajoshi/oasis3_bids/',subname,sessions{1},'anat',[subname,'_',sessions{1},'_T1w.nii.gz']);
     end
     
     if ~exist(fmri,'file')
-        fmri=fullfile('/ImagePTE1/ajoshi/oasis3_bids/',subname,sessions{1},'func',[subname,'_',sessions{1},'_task-rest_bold.nii.gz']);
+        fmri=fullfile('/ImagePTE1/ajoshi/data/oasis3_bids/',subname,sessions{1},'func',[subname,'_',sessions{1},'_task-rest_bold.nii.gz']);
         runname='run_00';
     else
         runname='run_02';
@@ -55,12 +52,21 @@ for j=3:length(l)
 end
 
 %Process all subjects using BFP
-parpool(6);
-parfor s = 1:length(subnamelist)
-    try
-        bfp(configfile, t1list{s}, fmrilist{s}, studydir, [subnamelist{s},'_',sessionslist{s},'_',runlist{s}], 'rest',TR);
-    catch 
-        fprintf('subject failed:%d  %s',s,subnamelist{s});
-    end
+%parpool(6);
+GOrdSurfIndFile='/home/ajoshi/projects/bfp/supp_data/bci_grayordinates_surf_ind.mat';
+out_dir='/ImagePTE1/ajoshi/data/thickness_data/thickness_ld';
+for s = 1:length(subnamelist)
+%    try
+        subid=[subnamelist{s},'_',sessionslist{s},'_',runlist{s}];
+        subdir=fullfile(studydir,subid);
+        anatDir=fullfile(subdir,'anat');
+        subbasename=fullfile(anatDir,sprintf('%s_T1w',subid));
+
+        GOrdFile=fullfile(out_dir,[subid,'.ld.gord.mat']);
+        linked_dist_gord(subbasename,GOrdSurfIndFile,GOrdFile);
+        %        bfp(configfile, t1list{s}, fmrilist{s}, studydir, [subnamelist{s},'_',sessionslist{s},'_',runlist{s}], 'rest',TR);
+ %   catch 
+        fprintf('subject done:%d  %s',s,subnamelist{s});
+  %  end
 end
 
